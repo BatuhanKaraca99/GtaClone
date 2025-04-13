f97f4d927a6151fd37f247d488162d31f29d55f4
Batuhan KARACA <b.karaca.1999@gmail.com>
2025-04-14 02:26:03 +0300

    Spawning Police Officers

---
+++ Assets/Scripts/PoliceNPC/Police2Spawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police2Spawner : MonoBehaviour
+{
+    public GameObject[] AIPrefab;
+    public int AIToSpawn;
+
+    private void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        int count = 0;
+        while (count < AIToSpawn)
+        {
+            int randomIndex = Random.Range(0, AIPrefab.Length);
+
+            GameObject obj = Instantiate(AIPrefab[randomIndex]);
+
+            Transform child = transform.GetChild(Random.Range(0, transform.childCount - 1));
+            obj.GetComponent<Police2WaypointNavigator>().currentWaypoint = child.GetComponent<Waypoint>();
+
+            obj.transform.position = child.position;
+
+            yield return new WaitForSeconds(5f);
+
+            count++;
+        }
+    }
+}
